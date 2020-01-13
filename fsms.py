d8daed6e72ef9b49a246660113567422284c9ff9
Pemula1 <59829400+Pemula1@users.noreply.github.com>
2020-01-13 23:46:54 +0700

    Update fsms.py

--- fsms.py
+++ fsms.py
@@ -5,14 +5,6 @@ from requests import Session
 import re, sys
 s = Session()
 
-try:
-	print("SMS Gratis by Xractz - IndoSec\nGunakan kode negara (ex: 62xxxxx29)")
-	no = int(input("No    : 6285877524868")) 
-	msg = input("Pesan : saya mau makan") 
-except:
-	print("\n\t* Cek nomermu atau pesanmu! *")
-	sys.exit()
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36',
     'Referer': 'http://sms.payuterus.biz/alpha/'
@@ -24,8 +16,8 @@ jml = re.findall(r'<span>(.*?) = </span>', bypass)[0]
 captcha = eval(jml.replace("x", "*").replace(":", "/"))
 
 data = {
-	'nohp':no,
-	'pesan':msg,
+	'nohp':6285877524868,
+	'pesan':selamat pagi,
 	'captcha':captcha,
 	'key':key
 }
